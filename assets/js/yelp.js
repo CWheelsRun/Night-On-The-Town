2f631171e5a45f8fa4ab9f5cd317854ebf2123a0
Alexandria Rogers <rogers@Alexandrias-MBP.attlocal.net>
2022-08-18 20:26:42 -0400

    made yelp page commit

---
+++ assets/js/yelp.js
@@ -0,0 +1,19 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+var userCity = []
+ //Form that asks user for their city
+var cityForm =  []
+//Container or the resturants
+var restList = document.getElementById("yelpRests")
+
+var yelpCall = []
+var lon = "";
+var lat = "";
+
