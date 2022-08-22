ec8ddf6956f79e6a585c9e669e2b82b8588e1142
Corey B <corey.bennett@live.com>
2022-08-21 20:21:44 -0400

    update to styling and layout

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -1,15 +1,7 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
 var userCity = []
  //Form that asks user for their city
 var cityForm =  []
-//Container or the resturants
+//Container for the resturants
 var restList = document.getElementById("yelpRests")
 
 var yelpCall = []
