425849609d1a540c9b5c79e8014c52879548c572
Corey B <corey.bennett@live.com>
2022-08-21 22:15:03 -0400

    resolving conflicts

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -1,3 +1,11 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 var userFormEl = document.querySelector("#user-form")
  //Form that asks user for their city
 var searchInputEl = document.querySelector("#userCity")
